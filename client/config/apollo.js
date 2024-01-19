1bfdc73f5d9db383edc0d2e3480892301b12fde2
Clifford William <ccliffordwilliam@gmail.com>
2024-01-19 11:59:03 +0800

    feat(client-env):env file

--- client/config/apollo.js
+++ client/config/apollo.js
@@ -1,10 +1,9 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import * as SecureStore from "expo-secure-store";
-
 const httpLink = createHttpLink({
   //   uri: "https://server.cliffordwilliam.tech/",
-  uri: "https://hkqwj0gq-3000.asse.devtunnels.ms/", // TODO: move this to .env? Or change each time?
+  uri: process.env.EXPO_PUBLIC_SERVER_URL, // TODO: move this to .env? Or change each time?
 });
 // add token to header
 const authLink = setContext(async (_, { headers }) => {
