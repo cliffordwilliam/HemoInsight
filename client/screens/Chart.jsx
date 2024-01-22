7e1b4200375f04cefc0ab6d298ad4e088116feb4
Adjip25 <adjipratama18@gmail.com>
2024-01-22 13:19:58 +0700

    edit: changed data

--- client/screens/Chart.jsx
+++ client/screens/Chart.jsx
@@ -3,39 +3,51 @@ import React from "react";
 import { Dimensions, Text, View } from "react-native";
 
 const ChartPage = () => {
-  const linedata = {
+  const glucoseResult = {
     labels: ["January", "February", "March", "April", "May", "June"],
     datasets: [
       {
-        data: [20, 45, 28, 80, 99, 43],
-        strokeWidth: 2, // optional
+        data: [87, 101, 90],
+        strokeWidth: 5, // optional
       },
     ],
   };
+  const tgResult = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        data: [231, 300, 100],
+        strokeWidth: 5, // optional
+      },
+    ],
+  };
+  const services = [glucoseResult, tgResult];
   return (
     <View>
       <Text>Bezier Line Chart</Text>
-      <LineChart
-        data={linedata}
-        width={Dimensions.get("window").width} // from react-native
-        height={220}
-        yAxisLabel={"$"}
-        chartConfig={{
-          backgroundColor: "#e26a00",
-          backgroundGradientFrom: "#fb8c00",
-          backgroundGradientTo: "#ffa726",
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
+      {services.map((service) => (
+        <LineChart
+          data={service}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          yAxisLabel={"$"}
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
             borderRadius: 16,
-          },
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
-      />
+          }}
+        />
+      ))}
     </View>
   );
 };
